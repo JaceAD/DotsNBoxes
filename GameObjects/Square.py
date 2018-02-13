52383e731f2698775105f69db76a927cc5185acd
Jace <dembskij0659@my.uwstout.edu>
2018-02-13 15:52:18 -0600

    add x and y coords to Square object and setup function names for draw functions in square. Still needs implementation

--- GameObjects/Square.py
+++ GameObjects/Square.py
@@ -4,14 +4,17 @@ Created on Wed Feb  7 15:04:19 2018
 
 @author: Student
 """
+import pygame
 
 class Square:
-    def __init__(self):
+    def __init__(self, x, y):
         self.top = False
         self.bottom = False
         self.left = False
         self.right = False
         self.owner = "unOwned"
+        self.x = x
+        self.y = y
     
     def getLeft(self):
         return self.left
@@ -23,6 +26,11 @@ class Square:
         return self.bottom
     def getOwner(self):
         return self.owner
+    def getX(self):
+        return self.x
+    def getY(self):
+        return self.y
+    
     
     def setLeft(self, leftBool):
         self.left = leftBool
@@ -34,3 +42,34 @@ class Square:
         self.bottom = bottomBool    
     def setOwner(self, newOwner):
         self.owner = newOwner
+    
+    def drawLeft(self, color):
+        #implement here
+      
+    def drawRight(self, color):
+        #implement here
+          
+    def drawTop(self, color):
+        #implement here
+      
+    def drawBottom(self, color):
+        #implement here
+        
+    def draw(self):
+        fillBox = 0
+        if(getLeft):
+            self.drawLeft()
+            fillBox +=1
+        if(getRight):
+            self.drawRight()
+            fillBox +=1
+        if(getTop):
+            self.drawTop()
+            fillBox +=1
+        if(getBottom):
+            self.drawBottom()
+            fillBox +=1
+        if(fillBox == 4):
+            #Draw box fill color based on owner of Square
+        
+    
